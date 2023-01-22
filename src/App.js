0640c155b06d05d58c1f518370da56e3889beada
Anh <vuonga1103@yahoo.com>
2023-01-22 00:12:21 -0500

    Conditionally position bear based on screen width

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import styled from 'styled-components'
+
+import hotAirBalloonBear from './images/hot-air-balloon-bear.png'
+
+const AppWrapper = styled.div({
+  height: '100vh',
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'center',
+})
+
+const BottomRightBear = styled.img({
+  position: 'absolute',
+  bottom: 0,
+  right: 0,
+  height: '60%',
+  margin: 24,
+  '@media (max-width: 768px)': {
+    display: 'none',
+  },
+})
+
+const TopBear = styled.img({
+  display: 'none',
+  '@media (max-width: 768px)': {
+    display: 'block',
+    margin: 24,
+    width: 250,
+    alignSelf: 'flex-start',
+  },
+})
+
 function App() {
-  return <div>hi</div>
+  return (
+    <AppWrapper>
+      <TopBear src={hotAirBalloonBear} alt="Bear With Hot Air Balloon" />
+
+      <BottomRightBear
+        src={hotAirBalloonBear}
+        alt="Bear With Hot Air Balloon"
+      />
+    </AppWrapper>
+  )
 }
 
 export default App
